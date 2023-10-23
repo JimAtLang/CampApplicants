a5b81e1f8f4a70d44b1ff332801ee3c3c11d0c69
us lab <us.lab@thelangschool.net>
2023-10-23 11:30:56 -0400

    processApplicant

--- ApplicantProcessor.java
+++ ApplicantProcessor.java
@@ -10,6 +10,17 @@ public class ApplicantProcessor {
      */
     public String processApplicant(Applicant applicant){
         // part 1: put in conditionals that will return the correct string as shown above
+        int age = applicant.getAge();
+        if(age >=4 && age <=8){
+            return "Sparrow";
+        } 
+        if(age >= 9 && age <= 13){
+            return "Robin";
+        }
+         if(age >= 14  age <= 18){
+            return "Eagle";
+        }
+       
         return null;
     }
 }
