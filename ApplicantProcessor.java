ca0d2bb8f389815e668fbb4911b3cfa1a1c2c66e
jimatlang <jim.stewart@thelangschool.org>
2023-10-23 10:38:14 -0400

    created camper class

--- ApplicantProcessor.java
+++ ApplicantProcessor.java
@@ -8,8 +8,20 @@ public class ApplicantProcessor {
      * for applicants age 14-18 return "Eagle"
      * for all other ages return null;
      */
-    public String processApplicant(Applicant applicant){
-        // part 1: put in conditionals that will return the correct string as shown above
-        return null;
+    public Camper processApplicant(Applicant applicant){
+        String type;
+        int age = applicant.getAge();
+        if(age >= 4 && age <=8)
+        {
+            type = "Sparrow";
+        } else if (age >= 9 && age <=13) {
+           type = "Robin";
+        } else if (age >= 13 && age <=18) {
+           type = "Eagle";
+        } else {
+            return null;
+        }
+        return new Camper(applicant.getFirstName(), applicant.getLastName(), applicant.getAge(), type);
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
        new Main();
@@ -7,17 +9,25 @@ public class Main {
         Ui ui = new Ui();
         ApplicantProcessor p = new ApplicantProcessor();
         boolean next = true;
+        ArrayList<Camper> campers = new ArrayList<>();
         while(next){
             Applicant a = ui.getApplicant();
-            String group = p.processApplicant(a);
+            Camper c = p.processApplicant(a);
             // part 2: put in a conditional that will print a more user-friendly response 
-            System.out.println(group);
+            System.out.println(c.getType());
+            if(c!=null)
+                campers.add(c);
             String s = ui.inputString("Do you want to process another applicant?");
             if(Character.toLowerCase(s.charAt(0)) == 'y'){
                 next = true;
             } else {
                 next = false;
             }
+        }
+
+        System.out.println("Name\t\tAge\tType");
+        for(Camper camper:campers){
+            System.out.println(camper.getFirstName()+" " + camper.getLastName() + "\t" + camper.getAge() + "\t" + camper.getType());
         }
     }
 }
