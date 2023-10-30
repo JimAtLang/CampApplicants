047be7ad14757ded100aba87add977998128c83f
Zavier Gaffin <zavier.gaffin@thelangschool.net>
2023-10-30 11:19:14 -0400

    Camper class

---
+++ Camper.java
@@ -0,0 +1,36 @@
+public class Camper {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String type;
+    public Camper(String firstName, String lastName, int age, String type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.type = type;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+}
