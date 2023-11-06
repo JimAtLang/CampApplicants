111673f41fafb684be711a245893cafe91f793cd
US Breakout <us.breakout@thelangschool.net>
2023-11-06 13:16:06 -0500

    useless comment

--- Camper.java
+++ Camper.java
@@ -1,31 +1,8 @@
-public class Camper {
-    private String firstName;
-    private String lastName;
-    private int age;
+public class Camper extends Person {
     private String type;
     public Camper(String firstName, String lastName, int age, String type) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+       super(firstName, lastName, age);
         this.type = type;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
     }
     public String getType() {
         return type;
