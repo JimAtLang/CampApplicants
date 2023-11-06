ddcc431edf4a47b2a295c072c0d4c9791ac32539
Zavier Gaffin <zavier.gaffin@thelangschool.net>
2023-11-06 12:25:05 -0500

    created Person class

--- Applicant.java
+++ Applicant.java
@@ -1,55 +1,15 @@
-public class Applicant {
-    private String firstName;
-    private String lastName;
-    private int age;
-    
-    public Applicant(String firstName, String lastName, int age){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+public class Applicant extends Person{
+    private boolean processed;
+    public boolean isProcessed() {
+        return processed;
+    }
+    public void setProcessed(boolean processed) {
+        this.processed = processed;
+    }
+    public Applicant(String firstName, String lastName, int age, boolean processed){
+        super(firstName, lastName, age);
+        this.processed = processed;
     }
     
-
-    /**
-     * @return String return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * @return String return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * @return int return the age
-     */
-    public int getAge() {
-        return age;
-    }
-
-    /**
-     * @param age the age to set
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
 
 }
